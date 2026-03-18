e98d948c7bfbacaa09c965cdbf9909fb5fa9e94b
nibtr <nhthieu0106@gmail.com>
2026-03-18 18:30:31 +0700

    token types

--- lexer.go
+++ lexer.go
@@ -11,3 +11,56 @@ type token struct {
 func (l *lexer) scanTokens() []token {
 	return []token{}
 }
+
+// tokenType enum
+type tokenType int
+
+const (
+	// single character
+	LEFT_PAREN tokenType = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+
+	// 1 or 2 character
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+
+	// literals
+	IDENTIFIER
+	STRING
+	NUMBER
+
+	// keywords
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+
+	EOF
+)
