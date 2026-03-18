307f409076f69471d61824d5d7e2cf87f7b33a67
nibtr <nhthieu0106@gmail.com>
2026-03-18 18:24:01 +0700

    setup error reporting

--- main.go
+++ main.go
@@ -7,31 +7,25 @@ import (
 	"os/signal"
 )
 
-func main() {
-	args := os.Args[1:]
-	l := len(args)
-
-	if l > 1 {
-		fmt.Println("Usage: ./goitr [script]")
-	} else if l == 1 {
-		runFile(args[0])
-	} else {
-		runPrompt()
-	}
+type program struct {
+	hadError bool
 }
 
 // runFile runs the interpreter for the file at `path`
-func runFile(path string) {
+func (p *program) runFile(path string) {
 	bytes, err := os.ReadFile(path)
 	if err != nil {
 		panic(fmt.Errorf("read file %s: %w", path, err))
 	}
 
-	run(string(bytes))
+	p.run(string(bytes))
+	if p.hadError {
+		os.Exit(65)
+	}
 }
 
 // runPrompt runs the interpreter for the current prompt from user
-func runPrompt() {
+func (p *program) runPrompt() {
 	scanner := bufio.NewScanner(os.Stdin)
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
@@ -52,16 +46,46 @@ func runPrompt() {
 			break
 		}
 
-		run(scanner.Text())
+		p.run(scanner.Text())
+		p.hadError = false // reset the error flag
 	}
 }
 
 // run executes the interpreter for a source
-func run(source string) {
+func (p *program) run(source string) {
 	l := &lexer{source: source}
 	tokens := l.scanTokens()
 
 	for _, token := range tokens {
 		fmt.Println(token)
+	}
+}
+
+// printError prints the error at line with msg
+func (p *program) printError(line uint32, msg string) {
+	p.report(line, "", msg)
+}
+
+// TODO: maybe an ErrorReporter interface to abstract
+// how we report the error ?
+
+// report reports the error at line and flag the program to have error
+func (p *program) report(line uint32, where string, msg string) {
+	fmt.Printf("[line %v] - error %v: %v", line, where, msg)
+	p.hadError = true
+}
+
+func main() {
+	args := os.Args[1:]
+	l := len(args)
+
+	if l > 1 {
+		fmt.Println("Usage: ./goitr [script]")
+	} else if l == 1 {
+		p := &program{}
+		p.runFile(args[0])
+	} else {
+		p := &program{}
+		p.runPrompt()
 	}
 }
