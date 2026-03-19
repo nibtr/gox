2a766ef6481de55b96ed8a8fa04c2f138f84bcc2
nibtr <nhthieu16@proton.me>
2026-03-19 14:06:54 +0700

    test

---
+++ lexer_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestLexer(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []token
+	}{
+		{
+			input: `!=;; // this is a comment`,
+			expected: []token{
+				newToken(BANG_EQUAL, "!=", nil, 1),
+				newToken(SEMICOLON, ";", nil, 1),
+				newToken(SEMICOLON, ";", nil, 1),
+				newToken(EOF, "", nil, 1),
+			},
+		},
+		{
+			input: `!=;; // this is a comment
+
+// this is a new line
++<=>=<>
+
+(( )) {}`,
+			expected: []token{
+				newToken(BANG_EQUAL, "!=", nil, 1),
+				newToken(SEMICOLON, ";", nil, 1),
+				newToken(SEMICOLON, ";", nil, 1),
+
+				newToken(PLUS, "+", nil, 4),
+				newToken(LESS_EQUAL, "<=", nil, 4),
+				newToken(GREATER_EQUAL, ">=", nil, 4),
+				newToken(LESS, "<", nil, 4),
+				newToken(GREATER, ">", nil, 4),
+
+				newToken(LEFT_PAREN, "(", nil, 6),
+				newToken(LEFT_PAREN, "(", nil, 6),
+				newToken(RIGHT_PAREN, ")", nil, 6),
+				newToken(RIGHT_PAREN, ")", nil, 6),
+				newToken(LEFT_BRACE, "{", nil, 6),
+				newToken(RIGHT_BRACE, "}", nil, 6),
+
+				newToken(EOF, "", nil, 6),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("Lexing", func(t *testing.T) {
+			l := newLexer(test.input)
+			tokens := l.scanTokens()
+
+			// compare tokens
+			for i, token := range tokens {
+				// check type
+				if token.tokenType != test.expected[i].tokenType {
+					t.Errorf("Token type mismatch at index %d: expected %v, got %v", i, test.expected[i].tokenType, token.tokenType)
+				}
+
+				// check lexeme
+				if token.lexeme != test.expected[i].lexeme {
+					t.Errorf("Token lexeme mismatch at index %d: expected %v, got %v", i, test.expected[i].lexeme, token.lexeme)
+				}
+
+				// check literal
+				if token.literal != test.expected[i].literal {
+					t.Errorf("Token literal mismatch at index %d: expected %v, got %v", i, test.expected[i].literal, token.literal)
+				}
+
+				// check line
+				if token.line != test.expected[i].line {
+					t.Errorf("Token line mismatch at index %d: expected %v, got %v", i, test.expected[i].line, token.line)
+				}
+
+			}
+
+		})
+	}
+}
