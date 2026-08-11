fa7515a520e086926a70722f10b2860869eedf7c
nibtr <nhthieu16@proton.me>
2026-08-11 21:41:25 +0700

    feat: class instance

--- runtime/class.go
+++ runtime/class.go
@@ -1,9 +1,28 @@
 package runtime
 
+import "fmt"
+
 type Class struct {
 	Name string
 }
 
 func (f *Class) String() string {
 	return f.Name
+}
+
+func (f *Class) Call(i *Interpreter, args []any) (any, error) {
+	instance := &ClassInstance{klass: f}
+	return instance, nil
+}
+
+func (f *Class) Arity() int {
+	return 0
+}
+
+type ClassInstance struct {
+	klass *Class
+}
+
+func (ci *ClassInstance) String() string {
+	return fmt.Sprintf("%v instance", ci.klass.Name)
 }
